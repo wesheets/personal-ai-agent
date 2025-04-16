b75eb52a93ca59b9d7bebedae9cd8812aa218055
wesheets <wesheets@gmail.com>
2025-04-16 08:30:40 -0500

    Update App.jsx

--- promethiosfrontend/src/App.jsx
+++ promethiosfrontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Splash from "./Splash";
-import ControlRoom from "./ControlRoom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AgentProvider } from './context/AgentContext'
+import Layout from './components/Layout'
+import StudentAsk from './pages/college/StudentAsk'
+import MemoryLog from './pages/college/MemoryLog'
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/dashboard" element={<ControlRoom />} />
-      </Routes>
-    </Router>
-  );
+    <AgentProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/college/ask" element={<StudentAsk />} />
+            <Route path="/college/memory" element={<MemoryLog />} />
+            <Route path="/" element={<div className="text-center p-10">Welcome to Promethios</div>} />
+          </Routes>
+        </Layout>
+      </Router>
+    </AgentProvider>
+  )
 }
+
+export default App
